41a7ccf06f5a579ef097f661d1971498f51a99f6
buehrerc <buehrerc@student.ethz.ch>
2023-05-22 18:15:45 +0100

    added unit tests for lending_protocol

--- test/test_lending_protocol.py
+++ test/test_lending_protocol.py
@@ -1,8 +1,7 @@
 import json
 from unittest import TestCase
 
-from envs.market_env.market import Market
-from envs.market_env.lending_protocol import LendingProtocol
+from envs.market_env.env import MultiAgentEnv
 
 CONFIG_FILE_PATH = r"./test_config/config_test.json"
 
@@ -11,12 +10,72 @@ class TestLendingProtocol(TestCase):
     def setUp(self) -> None:
         fs = open(CONFIG_FILE_PATH)
         self.config = json.load(fs)
-        self.market = Market(config=self.config)
-        self.lending_protocol = LendingProtocol(market=self.market, config=self.config)
+        fs.close()
+
+        self.env = MultiAgentEnv(config=self.config)
+        self.env.reset()
+        self.lending_protocol = self.env.lending_protocol
 
     def test_initialization(self):
-        pass
+        self.assertEqual(len(self.lending_protocol.plf_pools), 3)
+        self.assertEqual(len(self.lending_protocol.agent_balance), 2)
+        self.assertEqual(len(self.lending_protocol.supply_record), 0)
+        self.assertEqual(len(self.lending_protocol.borrow_record), 0)
 
+    def test_increase_collateral(self):
+        self.assertFalse(self.lending_protocol.update_collateral_factor(0, 1))
+        self.assertEqual(self.lending_protocol.plf_pools[0].get_collateral_factor(), 0.875)
 
+    def test_decrease_collateral(self):
+        self.assertFalse(self.lending_protocol.update_collateral_factor(0, -1))
+        self.assertEqual(self.lending_protocol.plf_pools[0].get_collateral_factor(), 0.825)
 
+    def test_increase_collateral_unallowed(self):
+        for i in range(5):
+            self.assertFalse(self.lending_protocol.update_collateral_factor(0, 1))
+        self.assertTrue(self.lending_protocol.update_collateral_factor(0, 1))
+
+    def test_deposit(self):
+        self.assertFalse(self.lending_protocol.deposit(1, 0, 100))
+        self.assertEqual(self.lending_protocol.agent_balance[1]['TKN'], 900)
+        self.assertEqual(len(self.lending_protocol.supply_record[(1, 0)]), 1)
+        self.assertEqual(self.lending_protocol.supply_record[(1, 0)][0][1], 100)
+
+    def test_deposit_not_enough_funds(self):
+        self.assertTrue(self.lending_protocol.deposit(1, 0, 1e10))
+
+    def test_withdraw(self):
+        self.assertFalse(self.lending_protocol.deposit(1, 0, 100))
+        self.assertFalse(self.lending_protocol.withdraw(1, 0, 100))
+        self.assertEqual(self.lending_protocol.agent_balance[1]['TKN'], 1000)
+        self.assertEqual(len(self.lending_protocol.supply_record[(1, 0)]), 0)
+
+    def test_withdraw_no_deposit(self):
+        self.assertTrue(self.lending_protocol.withdraw(1, 0, 1000))
+
+    def test_borrow(self):
+        self.assertFalse(self.lending_protocol.borrow(1, 0, 1, 100))
+        self.assertEqual(self.lending_protocol.agent_balance[1]['TKN'], 900)
+        self.assertEqual(self.lending_protocol.agent_balance[1]['USDC'], 7100)
+        self.assertEqual(len(self.lending_protocol.borrow_record[(1, 0, 1)]), 1)
+        self.assertEqual(self.lending_protocol.borrow_record[(1, 0, 1)][0][1], 100)
+
+    def test_borrow_not_enough_funds(self):
+        self.assertTrue(self.lending_protocol.borrow(1, 0, 1, 1e10))
+
+    def test_repay(self):
+        self.assertFalse(self.lending_protocol.borrow(1, 0, 1, 100))
+        self.assertFalse(self.lending_protocol.repay(1, 1, 0, 100))
+        self.assertEqual(self.lending_protocol.agent_balance[1]['TKN'], 1000)
+        self.assertEqual(self.lending_protocol.agent_balance[1]['USDC'], 5000)
+        self.assertEqual(len(self.lending_protocol.borrow_record[(1, 0, 1)]), 0)
+
+    def test_repay_no_loan(self):
+        self.assertTrue(self.lending_protocol.repay(1, 1, 0, 100))
+
+    def test_repay_not_enough_funds(self):
+        self.assertFalse(self.lending_protocol.borrow(1, 0, 1, 100))
+        # Hack agent balance
+        self.lending_protocol.agent_balance[1]['USDC'] = 0
+        self.assertTrue(self.lending_protocol.repay(1, 1, 0, 100))
 
