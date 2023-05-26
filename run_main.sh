a5e639065864245c218eebdea15a813a68224f36
buehrerc <buehrerc@student.ethz.ch>
2023-05-26 17:21:54 +0100

    remove gpu access for now

--- run_main.sh
+++ run_main.sh
@@ -15,7 +15,6 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
 #SBATCH --time=02:00:00
-#SBATCH --gres=gpu:1
 
 ################################################################################
 ##################### !!! DO NOT EDIT BELOW THIS LINE !!! ######################
