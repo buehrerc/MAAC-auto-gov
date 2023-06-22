49a5104d83b1b8c95208a35237056507fb083da4
buehrerc <buehrerc@student.ethz.ch>
2023-06-22 15:39:12 +0100

    original initialization

--- envs/market_env/plf_pool.py
+++ envs/market_env/plf_pool.py
@@ -45,11 +45,11 @@ class PLFPool:
         # Pool Parameters
         self.collateral_factor: float = collateral_factor
         self.col_factor_change_rate: float = col_factor_change_rate
-        # self.supply_token: Dict[str, float] = {PLF_INITIATOR: initial_starting_funds}
-        self.supply_token: Dict[str, float] = dict()
+        self.supply_token: Dict[str, float] = {PLF_INITIATOR: initial_starting_funds}
+        # self.supply_token: Dict[str, float] = dict()
         # Set the initial utilization ratio to the optimal utilization ratio
-        # self.borrow_token: Dict[str, float] = {PLF_INITIATOR: initial_starting_funds * PLF_OPTIMAL_UTILIZATION_RATIO}
-        self.borrow_token: Dict[str, float] = dict()
+        self.borrow_token: Dict[str, float] = {PLF_INITIATOR: initial_starting_funds * PLF_OPTIMAL_UTILIZATION_RATIO}
+        # self.borrow_token: Dict[str, float] = dict()
 
         # Interest Rate Model
         self.interest_change_rate = interest_change_rate
